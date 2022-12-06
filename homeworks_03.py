e38aa3636eefd8681c7a37049f5e54592c2b3c0e
Sviatoslav <soosanin2@gmail.com>
2022-12-06 14:33:57 +0200

    homework lesson 3

--- homeworks_03.py
+++ homeworks_03.py
@@ -1,8 +1,44 @@
-i = 1
-print(i)
-print('Hello World!')
-print("test_1")
-test_2 = "test_2"
-print(test_2)
-test_4 = "NEW_BRANCH_4"
-print(test_4)
+# 1
+print("I love Python" * 42)
+# ==================
+
+
+# 2
+age_in_month = 370
+print(age_in_month)
+# ==================
+
+
+# 3
+age_in_years = age_in_month // 12
+print(age_in_years)
+# ==================
+
+
+# 4
+my_name = "Sviatoslav"
+my_age = "My name is " + str(my_name) + " I'm " + str(age_in_years) + " years old"
+print(my_age)
+# ==================
+
+
+# 5
+first_variable = 1
+
+print(first_variable < 2)
+print(first_variable > 3)
+print(first_variable <= 4)
+print(first_variable >= 5)
+print(first_variable == 6)
+print(first_variable != 7)
+# ==================
+
+
+# 6
+a = 2
+b = 5
+c = 6
+d = str(a) + str(b) + str(c)
+print(d)
+print(type(d))
+# ==================
