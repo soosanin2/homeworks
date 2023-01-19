58a53cbdc1c6b37c97a72a2b34325bb0baa6d94d
Sviatoslav <soosanin2@gmail.com>
2023-01-19 16:31:06 +0200

    homevork 11 fixed return rez

--- homework_11/homework_11.py
+++ homework_11/homework_11.py
@@ -3,30 +3,29 @@
 
 import time
 
-
-def deco_time_and_name(func_to_deco):
-    def print_time_and_name(*args, **kwargs):
-        str_name = func_to_deco.__name__
+def deco(func):
+    def wrap(*args, **kwargs):
+        str_name = func.__name__
         print(f"function name - {str_name}")
         str_time = time.ctime()
         print(f"start function time - {str_time}")
-        func_to_deco(*args, **kwargs)
-        return
-    return print_time_and_name
+        rez = func(*args, **kwargs) + 3
+        return rez
+    return wrap
 
-@ deco_time_and_name
-def standard_func():
-    print("this is a standard function")
+@ deco
+def my_func(x, y):
+    return (x + y)
 
-standard_func()
+print(my_func(2, 4))
 
 
 
 # # 2 Написати кастомний Exception клас, MyCustomException, який
 # # має повідомляти "Custom exception is occured".
 
-class MyCustomException(Exception):
-    print("Custom exception is occured")
+# class MyCustomException(Exception):
+#     print("Custom exception is occured")
 
 
 # raise MyCustomException()
@@ -40,38 +39,58 @@ class MyCustomException(Exception):
 # # проте програма не має завершити своєї роботи.
 # #
 
-class MyManager:
-    def __enter__(self, *args, **kwargs):
-        print("==========")
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        if exc_type != None:
-            print(f'this exception: {exc_val}')
-
-        else:
-            pass
-
-        print("==========")
-
-
-        return True
-
-
-with MyManager():
-    some_text = "some_text"
-    print(some_text)
-
+# class MyManager:
+#     def __enter__(self, *args, **kwargs):
+#         print("==========")
+#     def __exit__(self, exc_type, exc_val, exc_tb):
+#         if exc_type != None:
+#             print(f'this exception: {exc_val}')
+#
+#         else:
+#             pass
+#
+#         print("==========")
+#
+#
+#         return True
+#
+#
+# with MyManager():
+#     some_text = "some_text"
+#     print(some_text)
+#
 
 
 # 4 Написати конструкцію try ... except ... else ... finally, яка буде робити точно те ж,
 # що і менеджер контексту із попереднього завдання.
 
-some_text = "some_text"
-try:
-    print("==========")
-    print(some_text)
+# some_text = "some_text"
+# try:
+#     print("==========")
+#     print(some_text)
+#
+# except Exception as oll_exception:
+#     print((f'this exception: {oll_exception}'))
+#
+# finally:
+#     print("==========")
 
-except Exception as oll_exception:
-    print((f'this exception: {oll_exception}'))
 
-finally:
-    print("==========")
+
+
+
+# @ deco_f
+# def standard_func():
+#     return  42
+#
+# standard_func()
+
+
+# def my_deco(func):
+#     def wrap():
+#         print('Start of decoration')
+#         print('End of decoration')
+#         rez = func() - 2
+#         return rez
+#     return wrap
+#
