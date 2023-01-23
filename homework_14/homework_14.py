37d19e9c30942f1d12a6fa07dffb0b1c44f75d02
Sviatoslav <soosanin2@gmail.com>
2023-01-23 20:55:06 +0200

    homevork 14 fixed_01

--- homework_14/homework_14.py
+++ homework_14/homework_14.py
@@ -1,10 +1,8 @@
 # 1. До завдання, в якому ви реалізовували телефонну книгу,
 # додати валідацію номера телефону за допомогою RegEx.
 # Врахувати можливість декількох форматів: +380XXXXXXXXX, 380XXXXXXXXX, 0XXXXXXXXX
-
+import re
 import json
-import time
-
 
 
 def ubd_dict(data_j):
@@ -47,22 +45,35 @@ while True:
 
     try:
         if user_input == "add":
-            add_input = input("enter a name and number: ")
-            add_split = add_input.split()
-            name = add_split[0]
-            num = int(add_split[1])
-            add_sub = phone_book.get(name)
-            if add_sub == None:
-                phone_book[name] = num
-                print("The subscribers " + add_split[0] + " is added to the phone book")
-                ubd_dict(phone_book)
+            try:
+                add_input = input("enter a name and number: ")
+                add_split = add_input.split()
+                name = add_split[0]
+                num =add_split[1]
+            except:
+                print("Invalid data entered, please try again")
 
-            else:
-                print("a subscriber with the name " + name + " already exists, try another name")
+            try:
+                v_namb = re.fullmatch(r'(0|380|\+380)(\d{9})', num)
+                str_v_namb = str(v_namb[0])
+                add_sub = phone_book.get(name)
+
+                if add_sub == None:
+                    phone_book[name] = str_v_namb
+                    print("The subscribers " + add_split[0] + " is added to the phone book")
+                    ubd_dict(phone_book)
+
+                else:
+                    print("a subscriber with the name " + name + " already exists, try another name")
+
+            except:
+                print("wrong number dialed")
+
 
         elif user_input == "stats":
             len_dct = str(len(phone_book))
             print("there are " + len_dct + " subscribers in this phone book")
+
 
         elif user_input == "list":
             sub_list = list(phone_book.keys())
@@ -76,6 +87,7 @@ while True:
 
         if phone_book == {}:
             print("Sorry, phone book is empty or you entered the wrong request")
+
 
         else:
             if user_input == "show":
@@ -96,6 +108,7 @@ while True:
                     ubd_dict(phone_book)
                 else:
                     print("The subscribers " + del_input + " does not exist")
+
 
     except ValueError:
         print("Sorry invalid request, try again")
