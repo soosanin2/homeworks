dc8a96d2aed59665aafe9f78caa7128a0b0ff696
Sviatoslav <soosanin2@gmail.com>
2023-02-12 18:39:49 +0200

    homevork 20 fixed

--- homework_18/homework_18.py
+++ homework_18/homework_18.py
@@ -1,12 +1,13 @@
 
 # 1. Створити клас Bot з атрибутом name та методами say_name та send_message.
-#
 # send_message має приймати параметри self і message і має друкувати message.
-#
 # Метод say_name має друкувати значення атрибуту name.
 
 class Bot:
-    name = "Bot"
+
+    def __init__(self):
+        self.name = "Bot"
+
 
     def say_name(self):
         print(self.name)
