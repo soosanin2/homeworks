4caa7b72e79f4ff1ea5ee74dd7854b93e8ce53b5
Sviatoslav_Adamov <57997746+soosanin2@users.noreply.github.com>
2023-04-10 16:24:18 +0300

    Update __init__.py

--- flaskProject/robot_app/__init__.py
+++ flaskProject/robot_app/__init__.py
@@ -7,12 +7,12 @@ from .config import AppConfig
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s %(levelname)s %(message)s")
 
-db = SQLAlchemy()
+
 app = Flask(__name__, template_folder='templates')
 
 app.config.from_object(AppConfig)
 
-db.init_app(app)
+
 
 from .views import *
 from .class_based_views import *
