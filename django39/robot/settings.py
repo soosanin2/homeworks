c7cb741a4fbc8241cb279ce611dbe5a2c10a328c
Sviatoslav_Adamov <57997746+soosanin2@users.noreply.github.com>
2023-05-23 12:10:34 +0300

    Update settings.py

--- django39/robot/settings.py
+++ django39/robot/settings.py
@@ -106,13 +106,6 @@ DATABASES = {
     }
 }
 
-# Database (SQLite)
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 
 
 # Password validation
